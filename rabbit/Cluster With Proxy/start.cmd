27f43da0080b087ae40e50adc1a37394bc0ca1b1
Marcos Miller Dantas <marcos.dantas@neon.com.br>
2022-01-05 15:58:41 -0300

    few fixes for first run

--- rabbit/Cluster With Proxy/start.cmd
+++
@@ -1,8 +0,0 @@
-start /wait cmd /k " docker build ./ -t haproxy:1.6 && exit"
-
-start /wait cmd /k "docker-compose up -d"
-
-start /wait cmd /k "docker exec -ti rabbitmq2 bash -c \"rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq1; rabbitmqctl start_app\""
-start /wait cmd /k "docker exec -ti rabbitmq3 bash -c \"rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq1; rabbitmqctl start_app\""
-start /wait cmd /k "docker exec -ti rabbitmq4 bash -c \"rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq1; rabbitmqctl start_app\""
-start /wait cmd /k "docker exec -ti rabbitmq1 bash -c \"rabbitmqctl set_policy ha-all '' '{\"ha-mode\":\"all\", \"ha-sync-mode\":\"automatic\"}'\""
